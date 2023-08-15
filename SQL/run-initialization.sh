f9840191051903db2e2a820e6de5c7dc726ffe00
Robin Potze <robin.potze@ordina.nl>
2023-08-15 22:45:12 +0200

    Changes compose to launch without all other dependencies, added debugging warning.

--- SQL/run-initialization.sh
+++ SQL/run-initialization.sh
@@ -1,5 +1,5 @@
 # Wait to be sure that SQL Server came up
-sleep 90s
+sleep 20
 
 # Run the setup script to create the DB and the schema in the DB
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P SQLDevPwd01! -d master -i MonsterDB.sql
