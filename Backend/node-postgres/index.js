3566bc5e6998bc4f79b0de6e6b04df1d9be846a7
Robin Potze <robin.potze@ordina.nl>
2023-08-16 20:39:07 +0200

    cleanup + express/Postgres prep

---
+++ Backend/node-postgres/index.js
@@ -0,0 +1,29 @@
+const express= require(`express`)
+const bodyParser= require(`body-parser`)
+const mysql= require(`mysql`)
+const cors= require(`cors`)
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+const app= express()
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to sql application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
