1653e9054024c25935826cab7f40a1b69e361bc5
Robin Potze <robin.potze@ordina.nl>
2023-08-17 11:56:25 +0200

    adds express structure and commands afaik

---
+++ Backend/src/database.js
@@ -0,0 +1,36 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'my_user',
+  host: 'localhost',
+  database: 'Monster',
+  password: 'SQLDevPwd01!',
+  port: 9229
+});
+
+const getQuest = () => {
+    return new Promise(function(resolve, reject) {
+      pool.query('SELECT * FROM Quests ORDER BY id ASC', (error, results) => {
+        if (error) {
+          reject(error)
+        }
+        resolve(results.rows);
+      })
+    }) 
+  }
+
+const sqlQuery = (body) => {
+  return new Promise(function(resolve, reject) {
+    const { userQuery } = body
+    pool.query('(userQuery)', [userQuery], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(`${results.rows}`)
+    })
+  })
+}
+  
+module.exports = {
+  getQuest,
+  sqlQuery,
+}
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,13 +1,15 @@
 const express= require(`express`)
 const bodyParser= require(`body-parser`)
-const mysql= require(`mysql`)
 const cors= require(`cors`)
+
+const app= express()
+const PORT = process.env.PORT || 8080;
+
+const database = require('./database')
 
 var corsOptions = {
   origin: "http://localhost:8081"
 };
-
-const app= express()
 
 app.use(cors(corsOptions));
 
@@ -17,13 +19,30 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.json())
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  next();
+});
+
+app.get('/', (req, res) => {
+  database.getQuest()
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to sql application." });
 });
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
