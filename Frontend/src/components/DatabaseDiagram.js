eed5b9122a0cac39f02560500592003e000d61b7
Robin Potze <robin.potze@ordina.nl>
2023-10-09 14:44:59 +0200

    Diagram fixes. for now.

--- Frontend/src/components/DatabaseDiagram.js
+++ Frontend/src/components/DatabaseDiagram.js
@@ -3,9 +3,18 @@ import React from 'react';
 import Draggable from 'react-draggable';
 
 export default () => {
+    function toggleDiagram() {
+        const diagramArea = document.getElementById('DiagramArea');
+        if (diagramArea.style.height != "0%") {
+            diagramArea.style.height = "0%";
+        } else {
+            diagramArea.style.height = "40%";
+        }
+    }
+
     return (
-    <div className='DatabaseDiagram'>
-        <button className='DrawerButton'>Database</button>
+    <div className='DatabaseDiagram' id="DiagramArea">
+        <button className='DrawerButton' onClick={toggleDiagram}>Database</button>
         <div className='DiagramArea'>
             <Draggable>
                 <img src='../img/DatabaseDiagram.png' className='DBDimg'/>
