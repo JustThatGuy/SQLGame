8515aafab5c123d7d63f5822315ad157bd06b5ef
Iris Groenewoudt <iris.groenewoudt@ordina.nl>
2023-11-19 20:34:58 +0100

    added button to hide the solution

--- Frontend/src/components/HintItem.js
+++ Frontend/src/components/HintItem.js
@@ -5,10 +5,14 @@ import React from 'react';
 import Collapsible from 'react-collapsible';
 
 export default function HintItem({ hint }) {
+
+  const [show, setShow] = React.useState(false);
+
   return (
     <Collapsible containerElementProps={hint.id} trigger={hint.title} transitionTime={100} easing='ease' contentHiddenWhenClosed='true'>
       <p className='HintDesc'>{hint.desc}</p>
-      <div className='HintSol'>
+      <button onClick={()=>setShow(!show)}>Show solution</button>
+      <div style={show?{display:"inline-block"}:{display:'none'}}className='HintSol'>
         <p className="HintSolText" toggleData="ToggledOff">{hint.sol}</p>
         <i className='HintSolHide'></i>
       </div>
